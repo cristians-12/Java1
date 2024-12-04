97070bfc881304fd0a88a64d713a6ad0c125bb3d
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-12-04 14:57:05 -0500

    primer commit

---
+++ src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int numero = 5;
+
+        // float o double para decimales, double es mas potente.
+        double decimales = 5.63434;
+
+        // se usa long para numeros muy grandes
+
+        System.out.println(numero);
+
+        String saludo = "Bienvenido, espero se encuentre bien";
+
+        System.out.println(saludo);
+
+        System.out.println(saludo.length());
+
+    }
+}
